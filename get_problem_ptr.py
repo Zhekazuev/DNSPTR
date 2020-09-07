0a41bf911933389f412f7c48c7b33d9fb675d9ba
Eugene Zuev <zhekazuev@gmail.com>
2020-09-07 20:17:38 +0300

    Add function for create output xlsx file from csv. Install pyexcel, pyexcel-xlsx, pyexcel-xlsxw

--- get_problem_ptr.py
+++ get_problem_ptr.py
@@ -5,19 +5,26 @@ http://jira/browse/PSSER-2588
 from dns import exception as dnsexception
 from dns import reversename
 from dns import resolver
+import pyexcel
 import csv
 
 
-def readfile(csv_file):
+def readfile(file_name, delimiter=","):
     """
      Read a CSV file using csv.DictReader
     """
-    csv_reader = csv.DictReader(csv_file, delimiter=',')
+    csv_reader = csv.DictReader(file_name, delimiter=delimiter)
     return csv_reader
 
 
+def create_xlsx_copy(file_name, delimiter=";"):
+    sheet = pyexcel.get_sheet(file_name=f"{file_name}.csv", delimiter=delimiter)
+    return sheet.save_as(f"{file_name}.xlsx")
+
+
 def main():
-    file = open("ptr_output.csv", "w",  encoding="utf8")
+    output_file_name = "ptr_output"
+    file = open(f"{output_file_name}.csv", "w",  encoding="utf8")
     file.write("NAME;MSISDN;IP;ARPA;DOMAIN;NEW_IP;ARPA_NEW;NEW_DOMAIN\n")
 
     with open('ptr_examples.csv', encoding="utf8") as csv_read:
@@ -35,14 +42,15 @@ def main():
             try:
                 domain = resolver.query(rev_addr, "PTR")[0]
             except resolver.NXDOMAIN:
-                domain = "Null"
+                domain = "NXDOMAIN"
             try:
                 new_domain = resolver.query(rev_addr, "PTR")[0]
             except resolver.NXDOMAIN:
-                new_domain = "Null"
+                new_domain = "NXDOMAIN"
             file.write(f"{row['NAME']};{row['MSISDN']};{row['FRAMED-IP-ADDRESS']};"
                        f"{rev_addr};{domain};{row['FRAMED-IP-ADDRESS NEW']};{new_rev_addr};{new_domain}\n")
     file.close()
+    create_xlsx_copy(output_file_name)
 
 
 if __name__ == '__main__':
